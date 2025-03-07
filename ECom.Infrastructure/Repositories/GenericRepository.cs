7461f4014f1f7c123ea6cc8fccc1509741418ca4
ahmet fikri yıldız <ahmet_6119@outlook.com.tr>
2025-03-07 18:01:31 +0300

    c

---
+++ ECom.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,37 @@
+﻿using ECom.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Infrastructure.Repositories
+{
+    public class GenericRepository<TEntity> : IGeneric<TEntity> where TEntity : class
+    {
+        public Task<TEntity> AddAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
